d465308f1698f9506e81fb117590167f6a6eab63
alvarojhr <alvaro.hernandez.2014@upb.edu.co>
2017-03-25 17:38:19 -0500

    Agregan imagenes y se empiezan a implementar los modelos

--- ferregistra/login/models.py
+++ ferregistra/login/models.py
@@ -1,3 +1,19 @@
-from django.db import models
+from ferregistraDB.db import models
 
 # Create your models here.
+
+class Empleado(models.Model):
+    cedula = models.IntegerField(max_length = 30)
+    nombre = models.CharField(max_length=30)
+    apellido = models.CharField(max_length=30)
+    fecha_nacimiento = models.DateField(max_length = 100)
+    direccion = models.CharField(max_length=30)
+    telefono =models.CharField(max_length=30)
+    celular= models.CharField(max_length=30)
+    email = models.EmailField
+    idCargo = models.ForeignKey (Cargo, on_delete=models.CASCADE)
+    activo = models.BooleanField
+    password = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.title
